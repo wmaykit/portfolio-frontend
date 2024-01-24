8d678c02b5d65c0f61417908c41b4f750e7ae9c0
Kabanyok, Artyom <a.kabanyok@itransition.com>
2024-01-24 16:46:51 +0300

    Completed the "about" page
    * Add photo
    * Add text
    * Layout
    * Registered in the router

--- src/app/portfolio.routes.ts
+++ src/app/portfolio.routes.ts
@@ -5,10 +5,12 @@ import { CorewarComponent } from './projects/corewar/components/corewar/corewar.
 import {
   CorewarCodeEditorComponent
 } from './projects/corewar/components/corewar-code-editor/corewar-code-editor.component';
+import { AboutComponent } from './core/pages/about/about.component';
 
 export const routes: Routes = [
   { path: 'push-swap', component: PushSwapComponent },
   { path: 'corewar', component: CorewarComponent },
   { path: 'corewar-code-editor', component: CorewarCodeEditorComponent },
+  { path: 'about', component: AboutComponent },
   { path: '', component: MainComponent }
 ]
